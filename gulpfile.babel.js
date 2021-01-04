14eda2cfe1076b389a0a8ae7a8a451b40a93b9c8
wglee0511 <ranazang@naver.com>
2021-01-04 20:16:35 +0900

    Update images to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,32 @@
-import gulp from "gulp";
+import gulp, { dest } from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import img from "gulp-image";
 
 const routes = {
     pug:{
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img:{
+        src: "src/img/*",
+        dest: "build/img"
     }
 };
 
 const pug = () =>  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
-
 const clean = () => del("build");
-
 const webserver = () =>  gulp.src("build").pipe(ws({livereload: true, open: true}));
+const image  = () => gulp.src(routes.img.src).pipe(img()).pipe(gulp.dest(routes.img.dest));
 
-const watch = () => gulp.watch(routes.pug.watch, pug);
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, image);
+}
 
 
 
-export const dev = gulp.series(clean, pug, webserver, watch);
+export const dev = gulp.series(clean, image, pug, webserver, watch);
 
